fa29d34121c8904fe15648e61bf7a82a3db0deb3
wclimaco2025 <william.climaco@outlook.com>
2025-06-14 23:34:07 -0600

    Se realizo el Ejercicio 1 de la guia

--- guia_1/src/main.js
+++ guia_1/src/main.js
@@ -1,1 +1,10 @@
 import './style.css'
+
+
+// Ejercicio 1
+let edad= prompt("¿Cuál es tu edad?");
+let mayorMenor = edad >= 18? "Eres mayor de edad" : "Eres menor de edad";
+console.log(mayorMenor);
+
+// Ejercicio 2
+let nombre = prompt("¿digita tu nombre es tu nombre?");
